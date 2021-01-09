f182ada845bf2cf883707f1082efadbfd55fec23
Galaxy-cosmo <70648261+Galaxy-cosmo@users.noreply.github.com>
2021-01-09 13:32:40 -0500

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,24 @@
+class Ball{
+    constructor(x, y,radius) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y,radius,options)
+        this.radius = radius;
+        this.image = loadImage("polygon.png");
+        this.trajectory =[];
+        World.add(world, this.body);
+        
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.radius, this.radius);
+        pop();
+      }
+}
